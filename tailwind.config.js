778bc0cc9adef934e78973032f62fdceb8f78f2e
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-03-11 20:17:16 +0000

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+      padding: {
+        '1/3: '33.33333%' ,
+        '2/3: '66.66666%' ,
+      }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 }
