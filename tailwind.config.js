818607818df8cccc1e0dde51262a8edf85008f5f
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-03-28 22:43:39 +0000

    added responsive nav menu for smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,18 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
 }
+
+module.exports = {
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  }
+}
